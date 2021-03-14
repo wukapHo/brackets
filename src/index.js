ddd56b617854a2c1563c5496a94f0913065303d2
wukapHo <xb0ksx@gmail.com>
2021-03-14 03:03:35 +0300

    docs: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let closeBrackets = [];
+  for (item of bracketsConfig) {
+    openBrackets.push(item[0]);
+    closeBrackets.push(item[1]);
+  }
+  
+  let Arr = str.split('');
+  let stack = [];
+  for (item of Arr) {
+    i = openBrackets.indexOf(item);
+    k = closeBrackets.indexOf(item);
+
+    if(i != -1) {
+      stack.push(i);
+    } else if (stack.length === 0 || stack[stack.length - 1] != k) {
+      return false;
+    } else {
+      stack.pop();
+    }
+
+    if(i === k && (stack[stack.length - 1] === i) && (stack[stack.length - 2] === i)) {
+      stack.pop();
+      stack.pop();
+    }
+  }
+  
+  return (stack.length === 0);
 }
